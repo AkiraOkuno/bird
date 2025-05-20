7e3a94de4d431de405a09608a48b6ffba19c4bcb
Alex Akira Okuno <akira.okuno@outlook.com>
2025-05-20 11:22:13 -0400

    Update google_places_utils.py

--- google_places_utils.py
+++ google_places_utils.py
@@ -7,7 +7,7 @@ API_KEY = os.environ["GOOGLE_API_KEY"]
 
 def get_place_images_for_country(country_name, max_photos=5):
     """
-    Search Google Places API for landmarks in the given country and extract up to `max_photos` images.
+    Search Google Places API for tourist spots in the given country and return up to `max_photos` image + info pairs.
     """
     search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
     photo_url_template = "https://maps.googleapis.com/maps/api/place/photo"
@@ -29,12 +29,19 @@ def get_place_images_for_country(country_name, max_photos=5):
 
     photos = []
     for place in results:
+        name = place.get("name", "Lugar desconhecido")
+        address = place.get("formatted_address", "")
         for photo in place.get("photos", []):
             ref = photo.get("photo_reference")
             if ref:
-                url = f"{photo_url_template}?maxwidth=1600&photoreference={ref}&key={API_KEY}"
-                photos.append(url)
+                image_url = (
+                    f"{photo_url_template}?maxwidth=1600&photoreference={ref}&key={API_KEY}"
+                )
+                photos.append({
+                    "image_url": image_url,
+                    "place_name": name,
+                    "address": address
+                })
             if len(photos) >= max_photos:
                 return photos
-
     return photos
