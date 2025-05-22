470da31bd47cc37c7e54bd93d99e93136ccc6b3d
Alex Akira Okuno <akira.okuno@outlook.com>
2025-05-21 23:38:05 -0400

    Create restful_countries_get_data.py

---
+++ misc/restful_countries_get_data.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+API_KEY = ""
+API_URL = "https://restfulcountries.com/storage/images/presidents/muhammadu-buharipxpjw98lcj.jpg"
+
+headers = {"Authorization": API_KEY}
+url = "https://restfulcountries.com/api/v1/countries"
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+# Save the data to a JSON file
+with open("countries_data.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
