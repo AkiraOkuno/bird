c65785fc00f679aabadb122c4856053d6eb8bba2
Alex Akira Okuno <akira.okuno@outlook.com>
2025-11-05 10:54:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ messages = []
 errors = []
 
 for filename in os.listdir(MODULES_DIR):
-    if filename.endswith(".py"):
-    #if filename in ["random_song.py"]:
+    #if filename.endswith(".py"):
+    if filename in ["random_song.py"]:
         module_name = filename[:-3]
         module_path = os.path.join(MODULES_DIR, filename)
 
